42df3956dd8f5ec84b365932b88ad431fb80cbd9
AngeloSoza <angelosoza@uamv.edu.ni>
2024-06-18 19:38:38 -0600

    Codigo del menu

---
+++ CodigoFuente/codigo/variables.h
@@ -0,0 +1,10 @@
+#define MAX_REGISTERS 100 //maximo de registros que se pueden usar
+
+typedef struct USUARIO{
+    char nombres [50];
+    char apellidos [50];
+    char id;
+    char telefono [10];
+    int cantPersonas;
+    char fecha [10];
+};
